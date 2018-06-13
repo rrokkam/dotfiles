a927a183b86822bba7f37cf0d20d7bb3fa2c9faf
rrokkam <rohith.rokkam@stonybrook.edu>
2018-06-13 08:22:03 -0600

    Add script to restore mouse sensitivity

---
+++ mouse-speed.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# only for saigonkick PC
+# In general, follow the instructions here: 
+# https://askubuntu.com/questions/205676/how-to-change-mouse-speed-sensitivity
+
+xinput --set-prop "DELL Laser Mouse" "Device Accel Velocity Scaling" 5
